0d2d9c912dabfd23bd92d3aadcbad1e8376c3c56
Muxammad <swe.muhammad@gmail.com>
2023-04-16 14:59:26 +0500

    appdbcontext updated

--- src/EcoEspresso.DataAccess/Context/AppDbContext.cs
+++ src/EcoEspresso.DataAccess/Context/AppDbContext.cs
@@ -15,4 +15,15 @@ public class AppDbContext : DbContext
     public DbSet<Meal> Meals { get; set; }
     public DbSet<MealInfo> mealInfos { get; set; }
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Meal>()
+            .HasOne<MealInfo>(m => m.MealInfo)
+            .WithMany(m => m.Meals)
+            .HasForeignKey(m => m.MealInfo.Id)
+            .HasForeignKey(m => m.Chef)
+            .OnDelete(DeleteBehavior.NoAction);
+
+    }
 }
