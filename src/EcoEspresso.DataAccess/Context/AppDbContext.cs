f42194b2653de7ee98f6e2e809c11a4addf28fb5
Muxammad <swe.muhammad@gmail.com>
2023-04-16 14:24:50 +0500

    DbContext added

---
+++ src/EcoEspresso.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EcoEspresso.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcoEspresso.DataAccess.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Admin> Admins { get; set; }
+    public DbSet<Chef> Chefs { get; set; }
+    public DbSet<Meal> Meals { get; set; }
+    public DbSet<MealInfo> mealInfos { get; set; }
+
+}
