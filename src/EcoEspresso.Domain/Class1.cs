49e2957e2c049dd5548ff4b00b28147d15482ec2
Muxammad <swe.muhammad@gmail.com>
2023-04-16 11:43:53 +0500

    MealType class added

--- src/EcoEspresso.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EcoEspresso.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/EcoEspresso.Domain/Entities/MelaType.cs
@@ -0,0 +1,11 @@
+﻿using EcoEspresso.Domain.Commons;
+using System.Reflection.PortableExecutable;
+
+namespace EcoEspresso.Domain.Entities
+{
+    public class MelaType : Auditable
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
