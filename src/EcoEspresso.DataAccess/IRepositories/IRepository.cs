22f7019061b337a22f4dbb9a06f2a0fdcd20fd34
Muxammad <swe.muhammad@gmail.com>
2023-04-17 11:24:38 +0500

    Repository Interface

---
+++ src/EcoEspresso.DataAccess/IRepositories/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using EcoEspresso.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace EcoEspresso.DataAccess.IRepositories
+{
+    public interface IRepository<TSource> where TSource : Auditable
+    {
+        ValueTask<TSource> InsertAsync(TSource source);
+        ValueTask<TSource> UpdateAsync(TSource source);
+        ValueTask<bool> DeleteAsync(Expression<Func<TSource,bool>> expression);
+        ValueTask<TSource> GetAsync(Expression<Func<TSource,bool>> expression);
+        IQueryable<TSource> GetAllAsync();
+        ValueTask SaveAsync();
+    }
+}
