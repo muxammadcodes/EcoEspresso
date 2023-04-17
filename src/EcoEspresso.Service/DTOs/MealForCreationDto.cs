fcc5353cbbb85c37f6c5a45fe405f4deed658652
Muxammad <swe.muhammad@gmail.com>
2023-04-17 12:12:04 +0500

    meal creation dto addded

--- src/EcoEspresso.Service/DTOs/MealForCreationDto.cs
+++ src/EcoEspresso.Service/DTOs/MealForCreationDto.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EcoEspresso.Service.DTOs
+﻿namespace EcoEspresso.Service.DTOs
 {
-    internal class MealForCreationDto
+    public class MealForCreationDto
     {
     }
 }
