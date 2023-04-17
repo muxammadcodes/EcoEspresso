83882391be2c1e4ca9be29378bb11db5dae582ce
Muxammad <swe.muhammad@gmail.com>
2023-04-17 12:18:52 +0500

    Chefs dto folder

--- src/EcoEspresso.Service/DTOs/FoodInformations/FoodInformationCreatDto.cs
+++ src/EcoEspresso.Service/DTOs/FoodInformations/FoodInformationCreatDto.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using EcoEspresso.Domain.Entities;
+using EcoEspresso.Domain.Enum;
 
 namespace EcoEspresso.Service.DTOs.FoodInformations
 {
-    internal class FoodInformationCreatDto
+    public class FoodInformationCreatDto
     {
+        public int MealId { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public FoodType Type { get; set; }
     }
 }
